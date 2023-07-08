81de245d8e7b66f5970d445a2f89287cab2cac20
Queite Schneider Castiglioni <queitesc@gmail.com.br>
2023-07-08 18:03:09 -0300

    feat: add classes Project and Task

---
+++ src/main/java/model/Project.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.util.Date;
+
+public class Project {
+	private int id;
+	private String name;
+	private String description;
+	private Date createdAt;
+	private Date updateddAt;
+	
+	public Project(int id, String name, String description, Date createdAt, Date updateddAt ) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.createdAt = createdAt;
+		this.updateddAt = updateddAt;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getUpdateddAt() {
+		return updateddAt;
+	}
+
+	public void setUpdateddAt(Date updateddAt) {
+		this.updateddAt = updateddAt;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Project [id=" + id + ", name=" + name + ", description=" + description +
+	           ", createdAt=" + createdAt + ", updatedAt=" + updateddAt + "]";
+	}
+
+	
+	
+}
