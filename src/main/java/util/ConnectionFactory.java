0c59d97a5c73ade0d69764fa43f99f594af6e387
Queite <queitesc@gmail.com>
2023-07-11 15:55:53 -0300

    fix: db driver

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.Connection;
 
 public class ConnectionFactory {
 	
-	public static final String  DRIVER =  "com.mysql.jdbc.Driver";
+	public static final String  DRIVER =  "com.mysql.cj.jdbc.Driver";
 	public static final String URL = "jdbc:mysql://localhost:3306/todo";
 	public static final String USER = "root";
 	public static final String PASS = "";
