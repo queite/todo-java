6b902bf1a2445aed1eb31c188fb48b4bc84f5797
Queite <queitesc@gmail.com>
2023-07-11 14:45:52 -0300

    test: testing projects

--- src/main/java/com/todo/todo/App.java
+++ src/main/java/com/todo/todo/App.java
@@ -1,9 +1,26 @@
 package com.todo.todo;
 
+import java.util.List;
+
+import controller.ProjectController;
+import model.Project;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+    	
+    	ProjectController projectController = new ProjectController();
+    	
+    	Project project = new Project();
+    	project.setName("Projeto teste");
+    	project.setDescription("uma descrição qualquer");
+    	projectController.save(project);
 
+    	project.setName("Segundo nome");
+    	projectController.update(project);
+    	
+    	List<Project> projects = projectController.getAll();
+    	System.out.println("Total de projetos " + projects.size());
+    	System.out.println(projects);
     }
 }
