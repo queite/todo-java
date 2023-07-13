d0b06187c48a77c765dfd185bcd504198acb71f6
Queite <queitesc@gmail.com>
2023-07-13 19:57:44 -0300

    feat: adds table in main screen

--- src/main/java/view/MainScreen.java
+++ src/main/java/view/MainScreen.java
@@ -4,10 +4,10 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
 import java.awt.Color;
-import java.awt.Rectangle;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.ImageIcon;
@@ -17,10 +17,20 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Component;
 import javax.swing.SwingConstants;
+import javax.swing.JList;
+import java.awt.Dimension;
+import javax.swing.ListSelectionModel;
+import javax.swing.AbstractListModel;
+import javax.swing.JLayeredPane;
+import javax.swing.JTable;
+import java.awt.CardLayout;
+import javax.swing.table.DefaultTableModel;
+import java.awt.BorderLayout;
 
 public class MainScreen extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -42,6 +52,7 @@ public class MainScreen extends JFrame {
 	 * Create the frame.
 	 */
 	public MainScreen() {
+		setMinimumSize(new Dimension(400, 600));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 543, 392);
 		contentPane = new JPanel();
@@ -49,124 +60,185 @@ public class MainScreen extends JFrame {
 
 		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(94, 0, 94));
-		panel.setLayout(null);
+		JPanel panelHeader = new JPanel();
+		panelHeader.setBackground(new Color(94, 0, 94));
+		panelHeader.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("ToDo App");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Queite\\eclipse-workspace\\todo\\src\\main\\resources\\check-mark-3-32.png"));
-		lblNewLabel.setForeground(new Color(244, 244, 244));
-		lblNewLabel.setFont(new Font("Segoe UI", Font.BOLD, 29));
-		lblNewLabel.setBounds(10, 11, 394, 36);
-		lblNewLabel.setBackground(new Color(255, 255, 255));
-		panel.add(lblNewLabel);
+		JLabel lableHeaderTitle = new JLabel("ToDo App");
+		lableHeaderTitle.setIcon(new ImageIcon("C:\\Users\\Queite\\eclipse-workspace\\todo\\src\\main\\resources\\check-mark-3-32.png"));
+		lableHeaderTitle.setForeground(new Color(244, 244, 244));
+		lableHeaderTitle.setFont(new Font("Segoe UI", Font.BOLD, 29));
+		lableHeaderTitle.setBounds(10, 11, 394, 36);
+		lableHeaderTitle.setBackground(new Color(255, 255, 255));
+		panelHeader.add(lableHeaderTitle);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(255, 255, 255));
-		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		JPanel panelProjects = new JPanel();
+		panelProjects.setBackground(new Color(255, 255, 255));
+		panelProjects.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		panel_2.setBackground(new Color(255, 255, 255));
+		JPanel panelTasks = new JPanel();
+		panelTasks.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panelTasks.setBackground(new Color(255, 255, 255));
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(255, 255, 255));
-		panel_3.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel panelListProjects = new JPanel();
+		panelListProjects.setBackground(new Color(255, 255, 255));
+		panelListProjects.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		panel_4.setBackground(new Color(255, 255, 255));
-		panel_4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		JPanel panelListTasks = new JPanel();
+		panelListTasks.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panelListTasks.setBackground(new Color(255, 255, 255));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(5)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 509, Short.MAX_VALUE)
+						.addComponent(panelHeader, GroupLayout.DEFAULT_SIZE, 509, Short.MAX_VALUE)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE)
+							.addComponent(panelProjects, GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 327, Short.MAX_VALUE))
+							.addComponent(panelTasks, GroupLayout.DEFAULT_SIZE, 327, Short.MAX_VALUE))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, 172, Short.MAX_VALUE)
-							.addGap(10)
-							.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, 327, Short.MAX_VALUE)))
+							.addComponent(panelListProjects, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(panelListTasks, GroupLayout.DEFAULT_SIZE, 326, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)))
 					.addGap(3))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(6)
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+					.addComponent(panelHeader, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
 					.addGap(11)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+						.addComponent(panelTasks, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panelProjects, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, 227, Short.MAX_VALUE)
-						.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, 227, Short.MAX_VALUE)))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(panelListTasks, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())
+						.addComponent(panelListProjects, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
 		);
+		panelListTasks.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblNewLabel_2 = new JLabel("Tarefas");
-		lblNewLabel_2.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_2.setForeground(new Color(64, 0, 64));
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"Nome", "Descricao", "Prazo", "Concluida"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class, Boolean.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			boolean[] columnEditables = new boolean[] {
+				false, false, false, true
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		table.setBounds(10, 11, 306, 413);
+		JScrollPane scrollPane = new JScrollPane(table);
+		panelListTasks.add(scrollPane);
 		
-		JLabel lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_4.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\Queite\\eclipse-workspace\\todo\\src\\main\\resources\\plus-16.png"));
-		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
-		gl_panel_2.setHorizontalGroup(
-			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panel_2.createSequentialGroup()
+		JList list = new JList();
+		list.setFixedCellHeight(40);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list.setSelectionBackground(new Color(151, 0, 151));
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setFont(new Font("Segoe UI", Font.BOLD, 13));
+		GroupLayout gl_panelListProjects = new GroupLayout(panelListProjects);
+		gl_panelListProjects.setHorizontalGroup(
+			gl_panelListProjects.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelListProjects.createSequentialGroup()
+					.addGap(161)
+					.addComponent(list, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(8))
+		);
+		gl_panelListProjects.setVerticalGroup(
+			gl_panelListProjects.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelListProjects.createSequentialGroup()
+					.addGap(9)
+					.addComponent(list, GroupLayout.DEFAULT_SIZE, 413, Short.MAX_VALUE)
+					.addGap(9))
+		);
+		panelListProjects.setLayout(gl_panelListProjects);
+		
+		JLabel labelTasks = new JLabel("Tarefas");
+		labelTasks.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		labelTasks.setForeground(new Color(64, 0, 64));
+		
+		JLabel labelTasksAdd = new JLabel("");
+		labelTasksAdd.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelTasksAdd.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		labelTasksAdd.setIcon(new ImageIcon("C:\\Users\\Queite\\eclipse-workspace\\todo\\src\\main\\resources\\plus-16.png"));
+		GroupLayout gl_panelTasks = new GroupLayout(panelTasks);
+		gl_panelTasks.setHorizontalGroup(
+			gl_panelTasks.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panelTasks.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 184, GroupLayout.PREFERRED_SIZE)
+					.addComponent(labelTasks, GroupLayout.PREFERRED_SIZE, 184, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, 103, Short.MAX_VALUE)
-					.addComponent(lblNewLabel_4)
+					.addComponent(labelTasksAdd)
 					.addContainerGap())
 		);
-		gl_panel_2.setVerticalGroup(
-			gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2.createSequentialGroup()
+		gl_panelTasks.setVerticalGroup(
+			gl_panelTasks.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelTasks.createSequentialGroup()
 					.addGap(5)
-					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNewLabel_2)
-						.addComponent(lblNewLabel_4))
+					.addGroup(gl_panelTasks.createParallelGroup(Alignment.LEADING)
+						.addComponent(labelTasks)
+						.addComponent(labelTasksAdd))
 					.addContainerGap())
 		);
-		panel_2.setLayout(gl_panel_2);
+		panelTasks.setLayout(gl_panelTasks);
 		
-		JLabel lblNewLabel_1 = new JLabel("Projetos");
-		lblNewLabel_1.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1.setForeground(new Color(64, 0, 64));
+		JLabel labelProjects = new JLabel("Projetos");
+		labelProjects.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		labelProjects.setForeground(new Color(64, 0, 64));
 		
-		JLabel lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\Queite\\eclipse-workspace\\todo\\src\\main\\resources\\plus-16.png"));
-		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
+		JLabel labelProjectsAdd = new JLabel("");
+		labelProjectsAdd.setHorizontalAlignment(SwingConstants.RIGHT);
+		labelProjectsAdd.setIcon(new ImageIcon("C:\\Users\\Queite\\eclipse-workspace\\todo\\src\\main\\resources\\plus-16.png"));
+		GroupLayout gl_panelProjects = new GroupLayout(panelProjects);
+		gl_panelProjects.setHorizontalGroup(
+			gl_panelProjects.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelProjects.createSequentialGroup()
 					.addGap(10)
-					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
+					.addComponent(labelProjects, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
-					.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+					.addComponent(labelProjectsAdd, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
+		gl_panelProjects.setVerticalGroup(
+			gl_panelProjects.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelProjects.createSequentialGroup()
 					.addGap(5)
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1))
+					.addGroup(gl_panelProjects.createParallelGroup(Alignment.LEADING)
+						.addComponent(labelProjectsAdd, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
+						.addComponent(labelProjects))
 					.addContainerGap())
 		);
-		panel_1.setLayout(gl_panel_1);
+		panelProjects.setLayout(gl_panelProjects);
 		contentPane.setLayout(gl_contentPane);
 	}
 }
