872873218ba37575b6c7bbf1ec473265f86d2dbc
Queite <queitesc@gmail.com>
2023-07-25 12:05:09 -0300

    feat: implement customized table cell renderer

---
+++ src/main/java/util/DeadlineColumnCellRenderer.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.util.Date;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import model.Task;
+
+public class DeadlineColumnCellRenderer extends DefaultTableCellRenderer{
+	
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+		
+		JLabel label = (JLabel) super.getTableCellRendererComponent(table,
+				value, isSelected, hasFocus, row, column);
+		label.setHorizontalAlignment(CENTER);
+		
+		TaskTableModel taskModel = (TaskTableModel) table.getModel();
+		Task task = taskModel.getTasks().get(row);
+		
+		if(task.getDeadline().after(new Date())) {
+			label.setBackground(Color.GREEN);
+		} else {
+			label.setBackground(Color.RED);
+		}
+		return null;
+	}
+}
